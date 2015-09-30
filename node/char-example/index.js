e5a09adae9e6056acf8ab71f07cf2f77d899797a
firkin <firkin@126.com>
2015-09-30 19:49:33 +0800

    fix

--- node/char-example/index.js
+++ node/char-example/index.js
@@ -7,8 +7,8 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  socket.on('send message', function(msg){
+    io.emit('back message', msg);
   });
 });
 
