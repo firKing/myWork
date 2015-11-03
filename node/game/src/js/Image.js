3b660cd76c7f186f18fe5e46c5549cc94c38254a
firk1n <firkin@126.com>
2015-11-03 23:08:11 +0800

    准备做计时器

--- node/game/src/js/Image.js
+++ node/game/src/js/Image.js
@@ -9,6 +9,10 @@ export default class Image extends Component {
         this.state = {selected: false};
         this.oldTarget = {};
         this.handleOnClick = this.handleOnClick.bind(this);
+
+        this.c = 0;
+        this.t = null;
+        this.gameBegin = this.gameBegin.bind(this);
     }
 
     // 把数据分行, 每行n个
@@ -40,6 +44,24 @@ export default class Image extends Component {
         this.state.selected = !this.state.selected;
     }
 
+    Print() {
+        console.log("我在打印!");  
+        this.t = setTimeout(this.Print(),1000);//开始计时器,调用自己,进行无穷循环  
+    }
+    // var timer;//该值表示要取消延迟执行的代码块
+  
+            //开始打印
+  
+    gameBegin() {  
+        this.Print();    //调用打印方法
+    }  
+  
+            //结束打印  
+  
+    StopPrint() {  
+        clearTimeout(this.t);    //取消计时器
+    }  
+
     render() {
         var imgs = this.props.data.map(function (item) {
             var src = item.value;
@@ -47,9 +69,14 @@ export default class Image extends Component {
                 <img key = {item.name} src={src} onClick = {this.handleOnClick} />
             );
         }, this);
+
         var imgsRandom = imgs.sort(this.randomSort);
+
+        var beginButton = (<button onClick = {this.gameBegin}>游戏开始</button>);
+
         return (
             <div className="tab-selector">
+                <div className='begin'>{beginButton}<p id='timer'>0</p></div>
                 {this.getUlImg(imgsRandom, 8)}
             </div>
         );
