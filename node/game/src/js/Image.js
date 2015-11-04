c1284600faaf49cf27e462be2324c982fc7732b1
firk1n <firkin@126.com>
2015-11-04 12:38:53 +0800

    分离计时器

--- node/game/src/js/Image.js
+++ node/game/src/js/Image.js
@@ -4,30 +4,22 @@ require('../css/style.css');
 
 export default class Image extends Component {
 
+    // 构造函数
     constructor() {
         super();
         this.state = {selected: false};
         this.oldTarget = {};
         this.handleOnClick = this.handleOnClick.bind(this);
-
-        this.total = 0;
-        this.tick = this.tick.bind(this);
-        this.gameBegin = this.gameBegin.bind(this);
-        this.clearTimer = this.clearTimer.bind(this);
     }
 
     // 把数据分行, 每行n个
     getUlImg(imgs, n) {
-        var result = [];
-        for (var i = 0; i < imgs.length; i+=n) {
-            var re = imgs.slice(i, i+n);
+        let result = [];
+        for (let i = 0; i < imgs.length; i+=n) {
+            let re = imgs.slice(i, i+n);
             result.push(<ul key = {i}>{re}</ul>);
         };
         return(result);
-    }
-
-    randomSort(a,b) {
-        return Math.random()>.5 ? -1 : 1;
     }
 
     // 处理图片点击事件
@@ -42,45 +34,22 @@ export default class Image extends Component {
             this.oldTarget = evt.target;
             this.oldTarget.setAttribute('class', 'selected');
         }
-
         this.state.selected = !this.state.selected;
     }
 
-    //计时器
-    tick() {
-        let p = document.getElementById('timer');
-        p.innerHTML = this.total;
-        this.total += 1;
-    }
-
-    // 游戏开始, 开始计时
-    gameBegin() {
-        this.total += 1;
-        this.interval = setInterval(this.tick, 1000);
-    }
-
-    // 终止计时
-    clearTimer() {
-         clearInterval(this.interval);
-    }
-
+    // 渲染函数
     render() {
-        var imgs = this.props.data.map(function (item) {
-            var src = item.value;
+        let imgs = this.props.data.map(function (item) {
+            let src = item.value;
             return (
                 <img key = {item.name} src={src} onClick = {this.handleOnClick} />
             );
         }, this);
-
-        var imgsRandom = imgs.sort(this.randomSort);
-
-        var beginButton = (<button onClick = {this.gameBegin}>开始计时</button>);
-
-        var endButton = (<button onClick = {this.clearTimer}>停止计时</button>)
-
+        let imgsRandom = imgs.sort(function(a, b){
+            return Math.random()>.5 ? -1 : 1;
+        });
         return (
             <div className="tab-selector">
-                <div className='begin'>{beginButton}{endButton}<p>用时 <span id='timer'>0</span> 秒</p></div>
                 {this.getUlImg(imgsRandom, 8)}
             </div>
         );
--- node/game/src/js/index.js
+++ node/game/src/js/index.js
@@ -4,7 +4,9 @@ import ReactDOM from 'react-dom';
 // import Test from './Test';
 import Image from './Image';
 import data from './data';
+import Timer from './Timer';
 
 
 // ReactDOM.render(<Test />, document.getElementById('root'));
-ReactDOM.render(<Image data ={data} />, document.getElementById('root'));
+ReactDOM.render(<Image data ={data} />, document.getElementById('imgSpace'));
+ReactDOM.render(<Timer/>, document.getElementById('timerSpace'));
